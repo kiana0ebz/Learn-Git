836d94f1cd770f0256b6983386392ad4ecb512c0
Hossein Karami <72271858+Hosseincpl@users.noreply.github.com>
2022-06-01 09:02:56 +0430

    Update person.py

--- project3/person.py
+++ project3/person.py
@@ -1,13 +1,9 @@
 import math
+from person import Person, Consts
 
-class Consts:
-    BASE_PRICE = {'worker': 100, 'teacher': 100, 'engineer': 200}
-    BASE_COST = {'worker': 200, 'teacher': 150, 'engineer': 300}
-    BASE_INCOME = {'worker': {'mine': 500}, 'teacher': {'mine': 200}, 'engineer': {'mine': 900}}
-    MIN_AGE = 15
-    AGE_MUL = 10
+# task assigned to ali
 
-class Person:
-  pass
+class Engineer:
+    pass
 
-# task assigned to you
+# task assigned to ali
