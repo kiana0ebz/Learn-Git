8c8f17b43d0e27856a53ddf8f43dcce5738a98f7
Hossein Karami <72271858+Hosseincpl@users.noreply.github.com>
2022-06-01 08:50:14 +0430

    Create person.py

---
+++ project3/person.py
@@ -0,0 +1,13 @@
+import math
+
+class Consts:
+    BASE_PRICE = {'worker': 100, 'teacher': 100, 'engineer': 200}
+    BASE_COST = {'worker': 200, 'teacher': 150, 'engineer': 300}
+    BASE_INCOME = {'worker': {'mine': 500}, 'teacher': {'mine': 200}, 'engineer': {'mine': 900}}
+    MIN_AGE = 15
+    AGE_MUL = 10
+
+class Person:
+  pass
+
+# task assigned to you
