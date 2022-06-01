b723b9a5806f413a45c4edc3cf2f5144257b7007
Hossein Karami <72271858+Hosseincpl@users.noreply.github.com>
2022-06-01 08:55:14 +0430

    Create teacher.py

---
+++ project3/teacher.py
@@ -0,0 +1,20 @@
+import math
+from person import Person, Consts
+
+
+class Teacher(Person):
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        super(Teacher, self).__init__(name, age)
+        self.job = "teacher"
+
+    def get_price(self):
+        return Consts.BASE_PRICE[self.job] - (self.age - Consts.MIN_AGE) * Consts.AGE_MUL
+
+    def calc_life_cost(self):
+        return Consts.BASE_COST[self.job] + (self.age - Consts.MIN_AGE) * Consts.AGE_MUL
+
+    def calc_income(self):
+        return Consts.BASE_INCOME[self.job][self.work_place.get_expertise()] - (
+                self.age - Consts.MIN_AGE) * Consts.AGE_MUL
