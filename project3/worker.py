b9f6808228ddbde31cf8294017e756be226f3e29
Hossein Karami <72271858+Hosseincpl@users.noreply.github.com>
2022-06-01 08:56:35 +0430

    Create worker.py

---
+++ project3/worker.py
@@ -0,0 +1,19 @@
+import math
+from person import Person, Consts
+
+
+class Worker(Person):
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        super(Worker, self).__init__(name, age)
+        self.job = "worker"
+
+    def get_price(self):
+        return math.floor(Consts.BASE_PRICE[self.job] * (Consts.MIN_AGE / self.age))
+
+    def calc_life_cost(self):
+        return math.floor(Consts.BASE_COST[self.job] * (self.age / Consts.MIN_AGE))
+
+    def calc_income(self):
+        return math.floor(Consts.BASE_INCOME[self.job][self.work_place.get_expertise()] * (Consts.MIN_AGE / self.age))
