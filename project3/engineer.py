91c5ec85f55ae94094c3793e1a9c8f6d29c1ccd3
kiana ebz <kimebzkimsth0@gmail.com>
2022-06-01 12:17:40 +0430

    done

--- project3/engineer.py
+++ project3/engineer.py
@@ -3,7 +3,19 @@ from person import Person, Consts
 
 # task assigned to ali
 
-class Engineer:
-    pass
+class Engineer(Person):
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+        super(Engineer, self).__init__(name, age)
+        self.job = "engineer"
 
-# task assigned to ali
+    def get_price(self):
+        return math.floor(Consts.BASE_PRICE[self.job] * math.sqrt(Consts.MIN_AGE / self.age))
+
+    def calc_life_cost(self):
+        return math.floor(Consts.BASE_COST[self.job] * math.sqrt(self.age / Consts.MIN_AGE))
+
+    def calc_income(self):
+        return math.floor(
+            Consts.BASE_INCOME[self.job][self.work_place.get_expertise()] * math.sqrt(Consts.MIN_AGE / self.age))
